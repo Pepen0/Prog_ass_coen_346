5186023c6821e481073c74b86860de1813e8c005
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-10-09 23:02:49 -0400

    Added recommanded change

--- unix-shell/parser.c
+++ unix-shell/parser.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/_types/_size_t.h>
+#include <sys/types.h>
 
 #define READ_CHAR *(*cur_buf)++ = *(*cur)++
 #define SKIP_CHAR (*cur)++
