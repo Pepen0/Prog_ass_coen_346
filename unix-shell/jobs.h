189ffc7fd08f1460e240ccdc89b0d059a6f01d15
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-10-11 21:39:20 -0400

    last commit hopefully

--- unix-shell/jobs.h
+++ unix-shell/jobs.h
@@ -14,7 +14,7 @@ struct job{
 void add_job(pid_t pid, char *cmdline);
 void update_jobs();
 void print_jobs();
-void remove_jobs(pid_t pid);
+void remove_jobs(pid_t pid); /* TODO: Allow jobs removal from the list*/
 void free_jobs();
 
 #endif // JOBS_H
