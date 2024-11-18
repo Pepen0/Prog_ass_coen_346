a953de4305bf3e57e98cb084cd95af7d16177eae
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-11-18 01:44:13 -0500

    Added atomic variable to limit race condition

--- Prog_ass_2/webserver/src/main/java/ca/concordia/server/Account.java
+++ Prog_ass_2/webserver/src/main/java/ca/concordia/server/Account.java
@@ -1,25 +1,35 @@
 package ca.concordia.server;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+//class to represent a bank account
 public class Account {
     //represent a bank account with a balance and withdraw and deposit methods
-    private int balance;
+    private AtomicInteger balance;
     private int id;
 
     public Account(int balance, int id){
 
-        this.balance = balance;
+        this.balance = new AtomicInteger(balance); // Allow atomic opperations on the balance
         this.id = id;
     }
 
+    // Method to get the balance
     public int getBalance(){
-        return balance;
+        return balance.get();
     }
 
+    // Method to withdraw
     public void withdraw(int amount){
-        balance -= amount;
+        //use compareAndSet to ensure that the balance is updated atomically
+        int currentBalance = balance.get();
+        balance.compareAndSet(currentBalance, currentBalance - amount);
     }
 
+    // Method to deposit
     public void deposit(int amount){
-        balance += amount;
+        //use addAndGet to ensure that the balance is updated atomically
+        balance.addAndGet(amount);
     }
 }
+
