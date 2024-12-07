a45639363a88bde41c38a1f15cc17f370f978ff9
Pepen0 <115287672+Pepen0@users.noreply.github.com>
2024-12-07 16:39:51 -0500

    full fork and wait logic

--- Visualisation code/Process.py
+++ Visualisation code/Process.py
@@ -4,7 +4,10 @@ class PCB:
     def __init__(self, pid, ppid=0, state= "New", runing_time=1):
         self.pid = pid
         self.ppid = ppid
+        self.cpid = 0
         self.state = state
+        self.iswaitingforchild = 0
+        self.iswaitingforchildpid = 0
         self.runing_time = runing_time
 
     def __str__(self):
@@ -24,6 +27,16 @@ class PCB:
     
     def get_pid(self):
         return self.pid
+    
+    def set_cpid(self, cpid):
+        self.cpid = cpid
+    
+    def get_waiting_status(self):
+        return self.iswaitingforchild, self.iswaitingforchildpid
+    
+    def set_waiting_status(self, iswaitingforchild, iswaitingforchildpid):
+        self.iswaitingforchild = iswaitingforchild
+        self.iswaitingforchildpid = iswaitingforchildpid
     
 
 class Process_table:
@@ -46,6 +59,9 @@ class Process_table:
 
     def get_processes(self):
         return self.processes
+    
+    def get_process_by_pid(self, pid):
+        return [process for process in self.processes if process.get_pid() == pid]
 
     def __str__(self):
         return "\n".join([str(process) for process in self.processes])
@@ -56,12 +72,41 @@ def create_process(process_table, running_time=1):
     print(process," Action: Created process")
     process_table.add_process(process)
 
-def fork_process(process_table, process_id, running_time=1):
+def fork_process(process_table, parent_id):
     pid = len(process_table.get_processes()) + 1
-    ppid = process_id
-    process = PCB(pid=pid, ppid=ppid, runing_time=running_time)
+    ppid = parent_id
+    parent_process = process_table.get_process_by_pid(parent_id)[0]
+    parent_process.set_cpid(pid)
+    process = PCB(pid=pid, ppid=ppid, runing_time=parent_process.get_runing_time())
     print(process," Action: Created process ",ppid, " child")
     process_table.add_process(process)
+
+def exec_process(process):
+    process.set_runing_time(process.get_runing_time() - 1)
+    #TODO: Add code to read code here
+    process.set_state("Waiting")
+
+def terminate_process(process_table, process):
+    process_parent = process_table.get_process_by_pid(process.get_pid())[0]
+
+    if process_parent.get_waiting_status()[1] == process.get_pid():
+        process_parent.set_waiting_status(0,0)
+    elif process_parent.get_waiting_status()[0] == 1 and process_parent.get_waiting_status()[1] == 0:
+        process_parent.set_waiting_status(0,0)
+
+    print(process," Action: Terminate process")
+    process_table.remove_process(process)
+    process.set_state("Terminated")
+
+def wait_process(process):
+    if process.cpid != 0:
+        process.set_state("Waiting")
+        process.set_waiting_status(1, 0)
+
+def wait_process_pid(parent_process, child_process):
+    if parent_process.cpid != 0:
+        parent_process.set_state("Waiting")
+        parent_process.set_waiting_status(1, child_process.get_pid())
 
 def Algorithm_1(process_table):
     for process in process_table.get_processes():
@@ -69,12 +114,15 @@ def Algorithm_1(process_table):
 
             if process.get_runing_time() > 0:
                 print(process," Action: Run process for 1 time unit")
-                process.set_runing_time(process.get_runing_time() - 1)
-                process.set_state("Waiting")
+                exec_process(process)
             elif process.get_runing_time() == 0:
-                print(process," Action: Terminate process")
-                process_table.remove_process(process)
-                process.set_state("Terminated")
+                if process.get_waiting_status()[0] == 0:
+                    terminate_process(process_table, process)
+                elif process.get_waiting_status()[1] != 0:
+                    print(process," Action: waiting for child process", process.get_waiting_status()[1])
+                else:
+                    print(process," Action: waiting for any child process")
+
             else:
                 print("Error: Process runing time is less than 0")
 
@@ -82,7 +130,6 @@ def run_process(process_table, total_runing_time):
     
     for i in range(total_runing_time):
         Algorithm_1(process_table)
-
 
 def switch_process(process_table, process_1_id, process_2_id):
     process_1 = process_table.get_processes(process_1_id)[0]
@@ -99,5 +146,5 @@ if __name__ == "__main__":
     total_runing_time = 20
     create_process(process_table)
     create_process(process_table,2)
-    fork_process(process_table,process_id=2,running_time=1)
+    fork_process(process_table=process_table,parent_id=2)
     run_process(process_table, total_runing_time)
